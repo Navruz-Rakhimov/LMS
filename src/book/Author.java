ff84fb5a176f944df41a5d24f42e1ce42dcc5a31
navruzrakhimov <rakhimovnavruz@gmail.com>
2021-01-08 19:32:31 +0500

    added view authors feature

---
+++ src/book/Author.java
@@ -0,0 +1,42 @@
+package book;
+
+public class Author {
+    private String authorId;
+    private String firstName;
+    private String lastName;
+
+    public Author(String authorId, String firstName, String lastName) {
+        this.authorId = authorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+}
