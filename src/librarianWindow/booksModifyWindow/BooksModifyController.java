8940215426b077f7af6e81c11ba135c46a753af7
navruzrakhimov <rakhimovnavruz@gmail.com>
2021-01-09 19:50:49 +0500

    librarian window (Azamat needs to modify)

---
+++ src/librarianWindow/booksModifyWindow/BooksModifyController.java
@@ -0,0 +1,62 @@
+package librarianWindow.booksModifyWindow;
+
+import book.Book;
+import book.BooksRepository;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
+public class BooksModifyController {
+    private Book book;
+
+    @FXML
+    private TextField isbnTextField;
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private TextField editionTextField;
+    @FXML
+    private TextField copyrightTextField;
+    @FXML
+    private TextField quantityTextField;
+
+    @FXML
+    public void initialize(){}
+
+    @FXML
+    public void setBookInfo(Book bookInfo){
+        book = bookInfo;
+
+        isbnTextField.setText(bookInfo.getIsbn());
+        titleTextField.setText(bookInfo.getTitle());
+        editionTextField.setText(bookInfo.getEdition());
+        copyrightTextField.setText(bookInfo.getCopyright());
+        quantityTextField.setText(String.format("%d", bookInfo.getQuantity()));
+    }
+
+    @FXML
+    public void modifyBookInfo(ActionEvent event){
+        Book editedBook = this.book;
+        editedBook.setCopyright(copyrightTextField.getText());
+        editedBook.setEdition(editionTextField.getText());
+        editedBook.setQuantity(Integer.parseInt(quantityTextField.getText()));
+        editedBook.setTitle(titleTextField.getText());
+        /*try {
+            BooksRepository.getInstance().updateBook(editedBook);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }*/
+        closeStage(event);
+    }
+
+    private void closeStage(ActionEvent event) {
+        Node source = (Node)  event.getSource();
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+}
---
+++ src/librarianWindow/librarianWindowController.java
@@ -0,0 +1,218 @@
+package librarianWindow;
+
+import adminWindow.NewBookDialogController;
+import adminWindow.NewUserDialogController;
+import authentication.UsersRepository;
+import book.Book;
+import book.BooksRepository;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import librarianWindow.booksModifyWindow.BooksModifyController;
+import librarianWindow.studentModifyWindow.StudentModifyWindowController;
+import staticTools.UserTracker;
+import user.User;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class librarianWindowController {
+    ObservableList<User> students;
+    ObservableList<Book> books;
+    @FXML
+    private Label userEmailLabel;
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private TableView<Book> booksTableView;
+    @FXML
+    private TableView<User> studentsTableView;
+
+
+    @FXML
+    public void initialize(){
+        userEmailLabel.setText(UserTracker.getLastTrackedUser());
+        books = BooksRepository.getInstance().getAllBooks();
+        students = UsersRepository.getInstance().getAllStudents();
+        booksTableView.setItems(books);
+        studentsTableView.setItems(students);
+    }
+
+    // books tab
+    @FXML
+    public void handleAddButton() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(gridPane.getScene().getWindow());
+
+        dialog.setTitle("Add New Book");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/adminWindow/newBookDialogFxml.fxml"));
+
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            NewBookDialogController controller = fxmlLoader.getController();
+
+            Book book = controller.getBook();
+
+            System.out.println("BOOK: " + book.getIsbn() + " " + book.getTitle() + " " + book.getEdition() + " " + book.getCopyright() + " " + book.getQuantity());
+
+            BooksRepository.getInstance().addBook(book);
+            // .....
+            if (containsIsbn(books, book.getIsbn())) {
+                books = BooksRepository.getInstance().getAllBooks();
+                booksTableView.setItems(books);
+            } else {
+                books.add(book);
+            }
+        }
+    }
+
+    @FXML
+    public void handleModifyButton() throws IOException, SQLException {
+        int addressIndex = booksTableView.getSelectionModel().getSelectedIndex();
+        Book book = books.get(addressIndex);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/librarianWindow/booksModifyWindow/booksModifyWindow.fxml"));
+        Parent parent = loader.load();
+        BooksModifyController dialogController = loader.<BooksModifyController>getController();
+        dialogController.setBookInfo(book);
+
+        Scene scene = new Scene(parent, 400, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Modify Book");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+        booksTableView.refresh();
+    }
+
+    @FXML
+    public void handleDeleteButton() {
+        try {
+            int addressIndex = booksTableView.getSelectionModel().getSelectedIndex();
+            Book book = books.get(addressIndex);
+            BooksRepository.getInstance().deleteBook(book);
+            books.remove(addressIndex);
+        } catch (Exception e) {
+            System.out.println("Select an item");
+        }
+    }
+
+    @FXML
+    public void handleSignOutButton(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/authentication/logInFxml.fxml"));
+            Node source = (Node) actionEvent.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // https://stackoverflow.com/questions/18852059/java-list-containsobject-with-field-value-equal-to-x
+    private boolean containsIsbn(final List<Book> list, final String isbn){
+        return list.stream().filter(o -> o.getIsbn().equals(isbn)).findFirst().isPresent();
+    }
+
+
+
+    // students tab
+    @FXML
+    public void handleAddStudentButton(ActionEvent actionEvent) {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(gridPane.getScene().getWindow());
+
+        dialog.setTitle("Add New Student");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/adminWindow/newUserDialogFxml.fxml"));
+
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            NewUserDialogController controller = fxmlLoader.getController();
+            User student = controller.getUser(2);
+            if (UsersRepository.getInstance().addUser(student)) {
+                student.setUserId(UsersRepository.getInstance().getUserId(student));
+                students.add(student);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Add New Student");
+                alert.setHeaderText("The student was not added!");
+                alert.setContentText("Student with entered email already exists!");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    @FXML
+    public void handleDeleteStudentButton(ActionEvent actionEvent) {
+        try {
+            int addressIndex = studentsTableView.getSelectionModel().getSelectedIndex();
+            User student = students.get(addressIndex);
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete Student");
+            alert.setHeaderText("Are you sure you want to delete the student: " + student.getFirstName() + " " + student.getLastName());
+            Optional<ButtonType> result  = alert.showAndWait();
+
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                UsersRepository.getInstance().deleteUser(student);
+                students.remove(addressIndex);
+            }
+        } catch (Exception e) {
+            System.out.println("Select an item");
+        }
+    }
+
+    @FXML
+    public void handleModifyStudentButton(ActionEvent act) throws IOException {
+        int addressIndex = studentsTableView.getSelectionModel().getSelectedIndex();
+        User user = students.get(addressIndex);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/librarianWindow/studentModifyWindow/studentModifyWindow.fxml"));
+        Parent parent = loader.load();
+        StudentModifyWindowController dialogController = loader.<StudentModifyWindowController>getController();
+        dialogController.setUserParams(user);
+
+        Scene scene = new Scene(parent, 400, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Modify Student Info");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+
+        studentsTableView.refresh();
+    }
+}
