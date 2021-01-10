cb37d584b6de2e38912be33382f9d35bd40ed072
navruzrakhimov <rakhimovnavruz@gmail.com>
2021-01-10 17:08:37 +0500

    fixed bugs

--- src/studentWindow/myBooksWindow/MyBooksWindowController.java
+++ src/studentWindow/myBooksWindow/MyBooksWindowController.java
@@ -1,29 +1,48 @@
 package studentWindow.myBooksWindow;
 
+import adminWindow.viewDialogs.BookViewController;
 import authentication.UsersRepository;
+import book.Book;
 import book.BooksRepository;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import user.Student;
+import user.User;
 
 import java.io.IOException;
 
 public class MyBooksWindowController {
 
+
     Student student;
+
+    ObservableList<Book> books;
+
+    @FXML
+    public GridPane gridPane;
+    @FXML
+    public TableColumn borrowedDateColumn;
+    @FXML
+    public TableColumn dueDateColumn;
     @FXML
     public TableView tableView;
 
     public void initialize() {
         String email = UsersRepository.getInstance().getCurrentUserEmail();
         student = UsersRepository.getInstance().getStudent(email);
-        tableView.setItems(BooksRepository.getInstance().getStudentBooks(student));
+        books = BooksRepository.getInstance().getStudentBooks(student);
+        tableView.setItems(books);
     }
 
     public void handleProfile(ActionEvent actionEvent) {
@@ -38,9 +57,7 @@ public class MyBooksWindowController {
         }
     }
 
-    public void handleMyBooks(ActionEvent actionEvent) {
-
-    }
+    public void handleMyBooks(ActionEvent actionEvent) {}
 
     public void handleOverdueBooks(ActionEvent actionEvent) {
         try {
@@ -76,5 +93,34 @@ public class MyBooksWindowController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void handleReturn(ActionEvent actionEvent) {
+        int addressIndex = tableView.getSelectionModel().getSelectedIndex();
+        Book book = books.get(addressIndex);
+        BooksRepository.getInstance().returnBook(student, book);
+        books.remove(book);
+    }
+
+    public void handleView(ActionEvent actionEvent) {
+        int addressIndex = tableView.getSelectionModel().getSelectedIndex();
+        Book book = books.get(addressIndex);
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(gridPane.getScene().getWindow());
+        dialog.setTitle("View Book");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/adminWindow/viewDialogs/bookViewDialogFxml.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        BookViewController controller = fxmlLoader.getController();
+        controller.setBook(book);
+        dialog.showAndWait();
     }
 }
--- src/studentWindow/overdueBooksWindow/OverdueBooksController.java
+++ src/studentWindow/overdueBooksWindow/OverdueBooksController.java
@@ -1,13 +1,21 @@
 package studentWindow.overdueBooksWindow;
 
+import adminWindow.viewDialogs.BookViewController;
 import authentication.UsersRepository;
+import book.Book;
 import book.BooksRepository;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import user.Student;
 
@@ -16,13 +24,21 @@ import java.io.IOException;
 public class OverdueBooksController {
 
     Student student;
+    ObservableList<Book> books;
 
+    @FXML
+    public Label totalFineLabel;
+    @FXML
+    public GridPane gridPane;
+    @FXML
     public TableView tableView;
 
     public void initialize() {
         String email = UsersRepository.getInstance().getCurrentUserEmail();
         student = UsersRepository.getInstance().getStudent(email);
-        tableView.setItems(BooksRepository.getInstance().getOverdueBooks(student));
+        totalFineLabel.setText(String.format("%.2f", BooksRepository.getInstance().getTotalFine(student)));
+        books = BooksRepository.getInstance().getOverdueBooks(student);
+        tableView.setItems(books);
     }
 
     public void handleProfile(ActionEvent actionEvent) {
@@ -76,8 +92,31 @@ public class OverdueBooksController {
     }
 
     public void handleView(ActionEvent actionEvent) {
+        int addressIndex = tableView.getSelectionModel().getSelectedIndex();
+        Book book = books.get(addressIndex);
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(gridPane.getScene().getWindow());
+        dialog.setTitle("View Book");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/adminWindow/viewDialogs/bookViewDialogFxml.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        BookViewController controller = fxmlLoader.getController();
+        controller.setBook(book);
+        dialog.showAndWait();
     }
 
     public void handleReturn(ActionEvent actionEvent) {
+        int addressIndex = tableView.getSelectionModel().getSelectedIndex();
+        Book book = books.get(addressIndex);
+        BooksRepository.getInstance().returnBook(student, book);
+        books.remove(book);
     }
 }
